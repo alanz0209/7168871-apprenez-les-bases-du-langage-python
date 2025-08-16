4d97fcd80460871e102161fd92ec483f934cfdcc
alanz0209 <alanizogo@gmail.com>
2025-08-16 14:01:33 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,15 @@
 # Écrivez votre code ici !
+nom = "John Doe"
+age = 30
+taille = 1.98
+est_etudiant = true
+
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est étudiant: {est_etudiant}")
+
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
